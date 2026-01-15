99bff8dda7941a457d3c9466a8e8a72c40df6576
Thorben Schirra <tls4@outlook.de>
2026-01-15 18:43:53 +0100

    still setting up google analytics. an error is shown saying the Google API cannot read the property id although it is being logged to the console.

--- app/functions/analytics.ts
+++ app/functions/analytics.ts
@@ -1,52 +1,46 @@
-import { BetaAnalyticsDataClient } from "@google-analytics/data"
+"use server"
 
-const googlePropertyId = process.env.GOOGLE_PROPERTY_ID!;
-const googleClientEmail = process.env.GOOGLE_CLIENT_EMAIL!;
-const googlePrivateKey = process.env.GOOGLE_PRIVATE_KEY!;
-
-if (!googleClientEmail || !googlePrivateKey) {
-    throw new Error("GOOGLE CREDENTIALS could not be found");
-}
-
-const analyticsClient = new BetaAnalyticsDataClient({
-    options: {
-        credentials: {
-            client_email: googleClientEmail,
-            private_key: googlePrivateKey
-        }
-    }
-});
+import { analyticsClient } from "../lib/clients/google";
 
 export async function runReport() {
+
+    const google = await analyticsClient();
+    const propertyId = process.env.GOOGLE_PROPERTY_ID;
+
+    if (!propertyId || propertyId === undefined) {
+        return {
+            body: "Google Property Id is missing",
+            status: 404,
+        }
+    }
+
+    console.log("Property ID: ", propertyId);
+
     try {
-        const report = await analyticsClient.runReport({
-            property: `properties/${googlePropertyId}`,
+        const response = await google.runReport({
+            property: `properties/${propertyId}`,
             dateRanges: [
                 {
-                    startDate: '2026-01-01',
-                    endDate: '2026-01-15'
-                }
-            ],
-            dimensions: [
-                {
-                  name: 'city',
+                  startDate: '2026-01-01',
+                  endDate: 'today',
                 },
-            ],
-            metrics: [
+              ],
+              metrics: [
                 {
-                    name: 'activeUsers'
+                  name: 'totalUsers',
                 },
-            ],
-        })
+              ],
+        });
 
+        const report = response;
         return {
             body: report,
             status: 200
         }
     } catch (error) {
         return {
-            body: `an error occurred while fetching the analytics data: ${error}`,
-            status: 500,
+            body: `runReport error: ${error}`,
+            status: 500
         }
     }
 }
---
+++ app/lib/clients/google.ts
@@ -0,0 +1,32 @@
+"use server"
+
+import { BetaAnalyticsDataClient } from "@google-analytics/data";
+
+export async function analyticsClient() {
+    const clientCredentials = process.env.GOOGLE_APPLICATION_CREDENTIALS;
+    const clientEmail = process.env.GOOGLE_CLIENT_EMAIL;
+    const privateKey = process.env.GOOGLE_PRIVATE_KEY;
+
+    if (
+        !clientCredentials || 
+        clientCredentials === undefined || 
+        !clientEmail || 
+        clientEmail === undefined || 
+        !privateKey || 
+        privateKey === undefined
+    ) {
+        throw new Error("google application credentials missing");
+    }
+
+    const client = new BetaAnalyticsDataClient({
+        credentials: {
+            client_email: clientEmail,
+            private_key: privateKey
+        }
+    });
+
+    client.runReport({
+        
+    })
+    return client;
+}

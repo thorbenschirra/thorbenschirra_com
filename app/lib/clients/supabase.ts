94e8bff5b69e806ec41d9fbb8fab7c2799fdec0c
Thorben Schirra <tls4@outlook.de>
2026-01-16 11:45:15 +0100

    optimization: exchanged hardcoded variable in the /app/lib/supabase.ts file and removed the client folder from the gitignore

--- app/lib/clients/supabase.ts
+++ app/lib/clients/supabase.ts
@@ -1,16 +1,42 @@
+"use server"
+
 import { createClient } from '@supabase/supabase-js';
 import { createBrowserClient } from '@supabase/ssr';
 
 export async function supabaseClient() {
+    const supabaseUrl = process.env.SUPABASE_URL;
+    const supabasePubKey = process.env.SUPABASE_PUBLISHABLE_KEY;
+
+    if (
+        !supabaseUrl || 
+        !supabasePubKey || 
+        supabasePubKey === undefined || 
+        supabaseUrl === undefined
+    ) {
+        throw new Error("supabase credentials are missing");
+    }
+
     return createClient(
-        "https://wfgiynuoemaseachwdhy.supabase.co",
-        "sb_publishable_HehSOGTi7sEPZdDIGMb_3g_MiAwugPD"
+        supabaseUrl,
+        supabasePubKey
     )
 }
 
 export async function supabaseBrowserClient() {
+    const supabaseUrl = process.env.SUPABASE_URL;
+    const supabasePubKey = process.env.SUPABASE_PUBLISHABLE_KEY;
+
+    if (
+        !supabaseUrl || 
+        !supabasePubKey || 
+        supabasePubKey === undefined || 
+        supabaseUrl === undefined
+    ) {
+        throw new Error("supabase credentials are missing");
+    }
+
     return createBrowserClient(
-        "https://wfgiynuoemaseachwdhy.supabase.co",
-        "sb_publishable_HehSOGTi7sEPZdDIGMb_3g_MiAwugPD"
+        supabaseUrl,
+        supabasePubKey
     )
 }
